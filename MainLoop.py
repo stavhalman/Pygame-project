b45d80cfe9840d2363c93c8bc3183ab50edceed2
stavhalman <50405478+stavhalman@users.noreply.github.com>
2022-03-24 19:13:12 +0200

    1.3.1

---
+++ MainLoop.py
@@ -0,0 +1,40 @@
+import pygame
+import Classes
+import Commands
+
+WINDOW_WIDTH = 1920
+WINDOW_HEIGHT = 1040
+pygame.init()
+size = (WINDOW_WIDTH, WINDOW_HEIGHT)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Game")
+clock = pygame.time.Clock()
+REFRESH_RATE = 60
+
+Player1 = Classes.Player([pygame.image.load('Charecter.R.png').convert(),
+                          pygame.image.load('Charecter.L.png').convert()],
+                         [pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_e,
+                          pygame.K_s, pygame.K_q], 400, 600, 100, 100, 0)
+Commands.cleanpictures(Player1.imgs)
+Player2 = Classes.Player([pygame.image.load('Charecter.R.png').convert(),
+                          pygame.image.load('Charecter.L.png').convert()],
+                         [pygame.K_h, pygame.K_k, pygame.K_u, pygame.K_i,
+                          pygame.K_j, pygame.K_y], 1400, 600, 100, 100, 1)
+Commands.cleanpictures(Player2.imgs)
+
+finish = False
+while not finish:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finish = True
+    Commands.loadbackground(screen, Player1, Player2)
+    keyPressedEvents = pygame.key.get_pressed()
+    Commands.keypressedevents(keyPressedEvents, Player1)
+    Commands.keypressedevents(keyPressedEvents, Player2)
+    Commands.gravity(Player1)
+    Commands.gravity(Player2)
+    Commands.groundhitbox(Player1)
+    Commands.groundhitbox(Player2)
+    clock.tick(REFRESH_RATE)
+
+pygame.quit()
